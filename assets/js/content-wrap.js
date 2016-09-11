db6cc8267313452cf5aa51418b85ca6a3e586aa0
Alim Babazade <alim095@mail.ru>
2016-09-11 13:52:28 +0500

    view collapse configure

--- assets/js/content-wrap.js
+++ assets/js/content-wrap.js
@@ -1,13 +1,54 @@
-//slider
-function contentWrapSlider(){
-	a = $("#content-wrap .course_list .panel-box").width();
-	console.log(a);
-	$("#content-wrap .course_list button").eq(2).click(function() {
+//kurslarin view olunmasi
+function contentWrapView(){
+	$("#content-wrap .courses_list .right_buttons .view_btn").click(function() {
 		console.log("a");
-		$("#content-wrap .course_list .panel-box").css({
-			position: "relative",
-			transform: "translateX(-"+a+")"
+		$(this).css({
+			display: "none"
+		});
+		$(this).siblings('button').eq(1).css({
+			display: "none"
+		});
+		$(this).siblings('button').eq(2).css({
+			display: "none"
+		});
+		$(this).siblings('button').eq(0).css({
+			display: "inline-block"
+		});
+		$(this).parent(".right_buttons").siblings('.sub-panel').css({
+			overflow: "visible"
+		});
+	});
+	$("#content-wrap .courses_list .right_buttons .collapse_btn").click(function() {
+		console.log("a");
+		$(this).css({
+			display: "none"
+		});
+		$(this).siblings('button').eq(1).css({
+			display: "inline-block"
+		});
+		$(this).siblings('button').eq(2).css({
+			display: "inline-block"
+		});
+		$(this).siblings('button').eq(0).css({
+			display: "inline-block"
+		});
+		$(this).parent(".right_buttons").siblings('.sub-panel').css({
+			overflow: "hidden"
 		});
 	});
 }
-contentWrapSlider();
+contentWrapView();
+
+//slider
+function contentWrapSlider(){
+	a = $("#content-wrap .courses_list .sub-panel").width();
+	console.log(a);
+	$("#content-wrap .courses_list button").eq(2).click(function() {
+		console.log("a");
+		$("#content-wrap .courses_list .panel-box").css({
+			transform: "translateX(-"+a+"px)",
+			transition: "all 0.4s ease"
+		});
+	});
+}
+// contentWrapSlider();
